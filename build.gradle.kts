29771eeb7cc76f2edd38cc86d4388596d73fba6f
jntakpe <jntakpe@gmail.com>
2017-09-22 23:20:51 +0200

    Use Kotlin instead of Groovy for gradle build script

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+import org.gradle.jvm.tasks.Jar
+
+val junitVersion = "5.0.0"
+val assertJVersion = "3.8.0"
+val commonsIOVersion = "2.5"
+
+group = "com.github.jntakpe"
+version = "0.1.0"
+
+buildscript {
+    repositories {
+        jcenter()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.1.50")
+        classpath("org.junit.platform:junit-platform-gradle-plugin:1.0.0")
+    }
+}
+
+plugins {
+    kotlin("jvm")
+}
+
+apply {
+    plugin("org.junit.platform.gradle.plugin")
+}
+
+dependencies {
+    compile(kotlin("stdlib"))
+    testCompile("commons-io:commons-io:$commonsIOVersion")
+    testCompile("org.assertj:assertj-core:$assertJVersion")
+    testCompile("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    jcenter()
+}
+
+val jar: Jar by tasks
+
+jar.apply {
+    manifest.attributes.apply {
+        put("Main-Class", "com.github.jntakpe.proxyswitcher.ProxySwitcherKt")
+    }
+    from(configurations.compile.map { if (it.isDirectory) it else zipTree(it) })
+}
